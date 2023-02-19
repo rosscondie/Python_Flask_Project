edefb817fabe6eaf0530724079c936eae6512af0
Ross Condie <rosscondie@gmail.com>
2023-02-19 10:44:20 +0100

    imported blueprint from controller to app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
+
+from controllers.countries_controller import countries_blueprint
 
 
 app = Flask(__name__)
 
-app.register_blueprint()
+app.register_blueprint(countries_blueprint)
 
 @app.route("/")
 def home():
