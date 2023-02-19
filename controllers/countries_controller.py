c2da2ea629ae28874bf7350cf1480654955a853e
Ross Condie <rosscondie@gmail.com>
2023-02-19 19:45:06 +0100

    added show all countries route to countries_controller

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -5,3 +5,8 @@ import repositories.country_repository as country_repository
 import repositories.city_repository as city_repository
 
 countries_blueprint = Blueprint("countries", __name__)
+
+@countries_blueprint.route("/countries")
+def countries():
+    countries = country_repository.select_all()
+    return render_template("countries/index.html", all_countries = countries)
--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -40,3 +40,8 @@ def select(id):
 def delete_all():
         sql = "DELETE  FROM countries"
         run_sql(sql)
+
+def delete(id):
+        sql = "DELETE  FROM countries WHERE id = %s"
+        values = [id]
+        run_sql(sql,values)
