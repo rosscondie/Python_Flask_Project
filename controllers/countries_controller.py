2b4a26964bc90c6816038408ea97e9b8e7e65910
Ross Condie <rosscondie@gmail.com>
2023-02-20 15:06:51 +0100

    added forms to countries/new.html

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
-
+from models.country import Country
 import repositories.country_repository as country_repository
 import repositories.city_repository as city_repository
 
@@ -19,6 +19,18 @@ def countries():
 @countries_blueprint.route("/countries/new")
 def new_country():
     return render_template("/countries/new.html")
+
+# CREATE
+# POST "/countries"
+@countries_blueprint.route("/countries", methods=['POST'])
+def create_country():
+    country_name = request.form['country_name']
+    country_continent = request.form['country_continent']
+    country_population = request.form['country_population']
+    country_language = request.form['country_language']
+    country = Country(country_name, country_continent, country_population, country_language)
+    country_repository.save(country)
+    return redirect("/countries")
 
 # SHOW 
 # GET "/countries/<id>"
