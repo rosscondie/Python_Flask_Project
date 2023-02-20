f870cb399f54c2a00c7e43e638d5776646aa561d
Ross Condie <rosscondie@gmail.com>
2023-02-20 17:01:58 +0100

    made a new_city route on controller

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
 from controllers.countries_controller import countries_blueprint
-from controllers.cities_controller import cities_blueprint
+
 
 app = Flask(__name__)
 
 app.register_blueprint(countries_blueprint)
-app.register_blueprint(cities_blueprint)
+
 
 
 # This route displays the homepage html file
--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -14,11 +14,20 @@ def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", all_countries = countries)
 
-# NEW
+# NEW COUNTRY ROUTE
 # GET "/countries/new"
 @countries_blueprint.route("/countries/new")
 def new_country():
     return render_template("/countries/new.html")
+
+# NEW CITY ROUTE
+# GET "/countries/<id>/new_city"
+@countries_blueprint.route("/countries/<id>/new_city")
+def new_city(id):
+    country_repository.select(id)
+    return render_template("/countries/new_city.html")
+
+
 
 # CREATE
 # POST "/countries"
